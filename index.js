51b1eab5f335f73b06a94b1f43de9ee4f24c89bb
bvekv1 <bvekp1@gmail.com>
2020-06-14 10:45:33 +0545

    bodyparser

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const cors = require ('cors');
 var morgan = require('morgan');
+var bodyParser = require('body-parser');
 const app = express();
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors());
-
+app.use(bodyParser.urlencoded({extended:true}));
 const userController = require('./controllers/userController');
 
 app.post('/registerUser',userController.register);
-app.get('/loginUser',userController.Login);
+app.post('/loginUser',userController.Login);
 
 app.listen('3000');
 console.log('server started');
